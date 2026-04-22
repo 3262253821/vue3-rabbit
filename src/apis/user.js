e04629ec5906008be9a4b4882d036a6fdfc1ada9
Hello World <chuanjianzhu1014@outlook.com>
2026-04-22 11:24:48 +0800

    update

--- src/apis/user.js
+++ src/apis/user.js
@@ -1,6 +1,6 @@
 // 封装所所有和用户相关的接口函数
 import httpInstance from "@/utils/http";
-
+// 登录接口
 // 使用对象的解构赋值,接收account和password
 export const loginAPI = ({ account, password }) => {
   return httpInstance({
